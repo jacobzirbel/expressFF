bb9292acc9f29c630dee63bad47839eec64c1491
Jacob Zirbel <jacobzirbel@gmail.com>
2020-05-30 22:23:10 -0500

    add a little html

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,3 @@
-const friends = [];
+const friends = [{ name: "Jacob", answers: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] }];
 
 module.exports = friends;
--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const path = require("path");
 const PORT = process.env.PORT || 3300;
 const apiRoutes = require("./app/routing/apiRoutes");
 const htmlRoutes = require("./app/routing/htmlRoutes");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "app/public")));
+
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
