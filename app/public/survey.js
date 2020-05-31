e3f74ea0fa11804190519d2f05396c0de360579b
Jacob Zirbel <jacobzirbel@gmail.com>
2020-05-30 22:24:23 -0500

    commit

--- app/public/survey.js
+++ app/public/survey.js
@@ -1,7 +1,7 @@
 const questions = [
-	"Question One",
-	"Question Two",
-	"Question Three",
+	"I prefer loud music",
+	"I like spending time with large groups of people",
+	"etc",
 	"Question Four",
 	"Question Five",
 	"Question Six",
@@ -40,12 +40,19 @@ form.appendChild(submit);
 
 function createSlider(questionID) {
 	let slider = document.createElement("div");
+	let agreeText = document.createElement("p");
+	let disagreeText = document.createElement("p");
+	agreeText.textContent = "Agree";
+	disagreeText.textContent = "Disagree";
 	slider.setAttribute("class", "slider");
 	let input = document.createElement("input");
 	input.setAttribute("type", "range");
 	input.setAttribute("min", "0");
 	input.setAttribute("max", "10");
 	input.setAttribute("name", questionID);
+
+	slider.appendChild(disagreeText);
 	slider.appendChild(input);
+	slider.appendChild(agreeText);
 	return slider;
 }
